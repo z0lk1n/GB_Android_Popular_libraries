71be99f8ba36bfb192164ec840bd86df69efe274
Vitaly <al-not@ya.ru>
2018-10-01 22:34:06 +0500

    add close progress dialog in presenter

--- app/src/main/java/com/example/vitaly/gb_android_popular_libraries/ui/githubparser/GitHubParserView.java
+++ app/src/main/java/com/example/vitaly/gb_android_popular_libraries/ui/githubparser/GitHubParserView.java
@@ -1,10 +1,10 @@
 package com.example.vitaly.gb_android_popular_libraries.ui.githubparser;
 
 import com.arellomobile.mvp.MvpView;
-import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
-@StateStrategyType(AddToEndSingleStrategy.class)
+@StateStrategyType(AddToEndStrategy.class)
 public interface GitHubParserView extends MvpView {
 
     void setUsernameText(String username);
