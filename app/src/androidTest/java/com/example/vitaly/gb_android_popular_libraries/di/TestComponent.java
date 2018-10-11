021354d4487038f7632ef1b88db59e8c7dfbd95c
Vitaly <al-not@ya.ru>
2018-10-11 19:56:06 +0500

    fix

--- app/src/androidTest/java/com/example/vitaly/gb_android_popular_libraries/di/TestComponent.java
+++ app/src/androidTest/java/com/example/vitaly/gb_android_popular_libraries/di/TestComponent.java
@@ -5,6 +5,7 @@ import com.example.vitaly.gb_android_popular_libraries.PaperDataCacheInstrumente
 import com.example.vitaly.gb_android_popular_libraries.RealmDataCacheInstrumentedTest;
 import com.example.vitaly.gb_android_popular_libraries.RealmImageCacheInstrumentedTest;
 import com.example.vitaly.gb_android_popular_libraries.UsersRepoInstrumentedTest;
+import com.example.vitaly.gb_android_popular_libraries.di.modules.AppModule;
 import com.example.vitaly.gb_android_popular_libraries.di.modules.ImageCacheModule;
 import com.example.vitaly.gb_android_popular_libraries.di.modules.RepoModule;
 
@@ -13,7 +14,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 @Singleton
-@Component(modules = {RepoModule.class, ImageCacheModule.class})
+@Component(modules = {RepoModule.class, ImageCacheModule.class, AppModule.class})
 public interface TestComponent {
     void inject(UsersRepoInstrumentedTest test);
 
